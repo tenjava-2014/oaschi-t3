e51d2d4381485533c4183fd0c45d6cc0d5789303
oaschi <aarooon@gmx.at>
2014-07-12 19:07:43 +0200

    rename of main class

--- src/main/java/com/tenjava/entries/oaschi/t3/Quantumizer.java
+++ src/main/java/com/tenjava/entries/oaschi/t3/Quantumizer.java
@@ -2,4 +2,8 @@ package com.tenjava.entries.oaschi.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class Quantumizer extends JavaPlugin {
+	
+	
+	
+}
